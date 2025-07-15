1d87de01462c57ff6b872cc1e925096b280ae7de
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-07-16 00:01:21 +0200

    Update uptime.py

--- commands/uptime.py
+++ commands/uptime.py
@@ -1,10 +1,11 @@
 from telegram import Update
-from telegram.ext import CallbackContext
+from telegram.ext import ContextTypes
 import time
 
-async def uptime(update: Update, context: CallbackContext):
+async def uptime(update: Update, context: ContextTypes.DEFAULT_TYPE):
     current_time = time.time()
-    uptime_seconds = int(current_time - context.bot_data.get("start_time", current_time))
+    start_time = context.bot_data.get("start_time", current_time)
+    uptime_seconds = int(current_time - start_time)
 
     days = uptime_seconds // 86400
     hours = (uptime_seconds % 86400) // 3600
