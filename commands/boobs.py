d16b4d2cbdc3aaee398bb813db80939da12dd023
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-07-16 12:25:51 +0200

    Update boobs.py

--- commands/boobs.py
+++ commands/boobs.py
@@ -1,22 +1,20 @@
+import aiohttp
 from telegram import Update
-from telegram.ext import CommandHandler, CallbackContext
-import requests
+from telegram.ext import ContextTypes
 
 API_KEY = "14960d2b4c71e3b190761233"
 API_URL = "https://api.lolhuman.xyz/api/nsfw/boobs"
 
-def boobs(update: Update, context: CallbackContext) -> None:
+async def boobs(update: Update, context: ContextTypes.DEFAULT_TYPE):
     params = {"apikey": API_KEY}
-    res = requests.get(API_URL, params=params)
-    if res.status_code == 200:
-        data = res.json()
-        image_url = data.get("result")
-        if image_url:
-            update.message.reply_photo(photo=image_url, caption="Voici tes boobs 😏")
-        else:
-            update.message.reply_text("Pas d'image retournée par l'API 😕")
-    else:
-        update.message.reply_text("Erreur lors de la requête à l'API.")
-
-boobs_handler = CommandHandler("boobs", boobs)
-dispatcher.add_handler(boobs_handler)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(API_URL, params=params) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                image_url = data.get("result")
+                if image_url:
+                    await update.message.reply_photo(photo=image_url, caption="Voici tes boobs 😏")
+                else:
+                    await update.message.reply_text("Pas d'image retournée par l'API 😕")
+            else:
+                await update.message.reply_text("Erreur lors de la requête à l'API.")
